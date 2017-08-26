cdb9c4330bdf6dfe9c7d3ffe1a755fe4086dd3af
Alex lipnik <alex.lipnik@gmail.com>
2017-08-26 18:07:53 +0300

    finished the basic design

--- client/components/mapContainer.jsx
+++ client/components/mapContainer.jsx
@@ -32,29 +32,71 @@ export class MapContainer extends React.Component {
     }
   }
 
+  markers = (data) => {
+
+    var lat,lng,name,time,id=0;
+
+    var arr = [   
+    ]
+
+    for(var j in data){
+      var temp = data[j];
+      for(var i in temp){
+        if(i === 'client_id'){
+          name = temp[i];
+        }
+        if(i === 'time'){
+          time = temp[i];
+        }
+        if(i === 'location'){
+          lng = temp[i].coordinates[0];
+          lat = temp[i].coordinates[1];
+          arr.push([
+              {lat: lat, lng: lng},
+              name,
+              time,
+              id++
+            ]
+          )
+        }
+      }
+    }
+
+    return  arr.map((v, i) => {
+      return (
+        <Marker
+          id ={i}
+          onClick={this.onMarkerClick}
+          title={'The marker`s title will appear as a tooltip.'}
+          name={"client_id: "+ v[1] }
+          time={"time: " + v[2] }
+          position={v[0]}
+          icon ={'http://maps.google.com/mapfiles/ms/icons/green-dot.png'}
+        />  
+      )
+    })
+  }
+
   render() {
     const { data, ...rest } = this.props;
     return (
-  		<Map style={style} zoom={14} {...rest} >
+  		<Map
+        center={{ lat: 43.363882, lng: -90.044922 }}
+        style={style}
+          zoom={7}
+        {...rest} 
+      >
         {
-          console.log(data)
+          this.markers(data)
         }
-  			<Marker
-          className={styles.marker}
-          onClick={this.onMarkerClick}
-		      title={'The marker`s title will appear as a tooltip.'}
-		      name={'SOMA'}
-		      position={{lat: 37.778519, lng: -122.405640}}
-          icon ={'http://maps.google.com/mapfiles/ms/icons/green-dot.png'}
-          />
-
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
-          </InfoWindow>
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h5>{this.state.selectedPlace.name}</h5>
+              <h5>{this.state.selectedPlace.time}</h5>
+            </div>
+        </InfoWindow>
       </Map>
     );
   }
