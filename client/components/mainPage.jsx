b5e858ff38b0feaf847564a9e48e200a6dba3b48
Alex lipnik <alex@localhost.localdomain>
2017-09-13 11:46:09 +0300

    initial

--- client/components/mainPage.jsx
+++ client/components/mainPage.jsx
@@ -1,100 +1,25 @@
 import React from 'react';
 import $ from 'jquery';
 import styles from './app.scss';
-import Map from './mapContainer.jsx';
 import { DatePicker, TimePicker, RaisedButton, AppBar, Drawer } from 'material-ui';
 import { Button } from 'react-bootstrap';
 import moment from 'moment';
 
 export default class MainPage extends React.Component {
 	state ={
-		date: new Date(2017, 2, 31),
-		time: new Date(0, 0, 0, 23, 3, 38),
-		activeUsersData: [],
-		inactiveUsersData: [],
-		open: false,
 	}
 
-	handleToggle = () => this.setState({open: !this.state.open});
-
-	updateDate = (event, value) => {
-		this.setState({
-			date: value
-		});
-	}
-
-	updateTime = (event, value) => {
-		this.setState({
-			time: value
-		});
-	}
-
-	handleClick = () => {
-		var year = this.state.date.getFullYear().toString();
-		var month = this.state.date.getMonth().toString();
-		var day = this.state.date.getDate().toString(); 
-		var hours1 = this.state.time.getHours();
-		var hours2 = hours1 + 2;
-		var minutes = this.state.time.getMinutes().toString();
-		var seconds = this.state.time.getSeconds().toString(); 
-
-		var _date1 = moment(new Date(year, month, day, hours1, minutes, seconds)).format('YYYY-MM-DD[T]HH:mm:ss[Z]')
-		var _date2 = moment(new Date(year, month, day, hours2, minutes, seconds)).format('YYYY-MM-DD[T]HH:mm:ss[Z]')
-
-		$.ajax({
-			url: '/get-geo-data',
-			data: {from: _date1, to: _date2},
-			success: ([tag, geo]) => { 
-				const activeUsers = [];
-				const inactiveUsers = [];
-				const activeIds = tag.map(v => v.client_id);
-				geo.forEach(g => {
-					if(activeIds.includes(g.client_id)){
-						activeUsers.push(g);
-					}
-					else{
-						inactiveUsers.push(g);
-					}
-				})
-				this.setState({
-					inactiveUsersData: inactiveUsers,
-					activeUsersData: activeUsers
-				})
-			},
-			dataType: 'json',
-		})
-	}
 
 	render() {
 		return (
 			<div className={styles.container}>
 				<div className={styles.control}>
-					<RaisedButton
-						className={styles.raisedButton}
-						label="Time and Date"
-						onClick={this.handleToggle}
-					/>
-					<Drawer width={200} openSecondary={true} open={this.state.open} >
-						<AppBar onClick={this.handleToggle} title="AppBar" />
-						<div className={styles.dateControl}>
-							<DatePicker onChange={this.updateDate} value={this.state.date} />
-							<div className={styles.time}>
-								<TimePicker
-									hintText="Select Time"
-									autoOk={true}
-									format='ampm'
-									onChange={this.updateTime}
-									value={this.state.time}
-								/>		
-							</div>
-							<Button className={styles.sendButton} bsStyle="primary" block onClick={this.handleClick}>Send</Button>
-						</div>
-					</Drawer>
-				</div>
-				<div className={styles.map_control}>
-					<div className={styles.map}>
-						<Map active={this.state.activeUsersData} inactive={this.state.inactiveUsersData} />
+					<div className={styles.product}>
+						<div className={styles.main-picture-container}></div>
+						<div className={styles.brif}></div>
+						<div className={styles.product-story}></div>
 					</div>
+					<div className={styles.information}></div> 
 				</div>
 			</div>
 		)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,41 +1,9 @@
 var path = require('path');
 var router = require('express').Router();
-var mongo = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
-
-var url = 'mongodb://localhost:27017/mydb';
 
 router.get('/', function (req, res) {
 	res.sendFile(path.resolve('../../client/index.html'));
-});
-
-router.get('/get-geo-data', function (req, res, data) {
-	mongo.connect(url, function(err, db) {
-		if(err){
-			return res.status(500).send("Couldn't connect to server")
-		}
-		var geoData;
-		var tagData;
-
-		const sel = {'time': {$gte: new Date(req.query.from), $lt: new Date(req.query.to)}};
-		var cursor1 = db.collection('geo').find(sel).toArray((err, geos) => {
-			if(err){
-				return res.status(500).send("Couldn't fetch documents")
-			}
-			geoData = geos;
-		});
-
-		var cursor2 = db.collection('tag').find(sel).toArray((err, tags) => {
-			if(err){	
-				return res.status(500).send("Couldn't fetch documents")
-			}
-			tagData = tags;
-			res.send(JSON.stringify([tagData, geoData]));
-		});
-
-		db.close();
-	});
 });
 
 module.exports = router;
