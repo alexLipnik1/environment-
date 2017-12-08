97ee48789fc5017d41d2a4574e8c1d1060b02e08
alex lipnik <alex.lipnik111@gmail.com>
2017-12-08 15:03:13 +0200

    initial

--- client/components/mainPage.jsx
+++ client/components/mainPage.jsx
@@ -12,20 +12,7 @@ export default class MainPage extends React.Component {
 
 	render() {
 		return (
-			<div className={styles.container}>
-				<div className={styles.control}>
-					<div className={styles.product}>
-						<div className={styles.mainPictureContainer}>
-							<h1>product Name</h1>
-							<div className={styles.picture}>
-								<img clasName={styles.imgProporations} src="https://ih0.redbubble.net/image.202496415.4788/flat,800x800,075,f.u2.jpg"/>
-							</div>
-						</div>
-						<div className={styles.brif}></div>
-						<div className={styles.productStory}></div>
-					</div>
-					<div className={styles.information}></div> 
-				</div>
+			<div>
 			</div>
 		)
 	}
